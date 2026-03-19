f9fe7357fbb07972d2c8ea2e5b5845b7a0581ae1
Guilherme-G <142443633+Guilherme-G@users.noreply.github.com>
2026-03-19 00:29:16 +0000

    aula 18/03/26

---
+++ src/form.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <h1>form - 18/03/26</h1>
+    <form method="post" action="recebe.php"> <!-- method = método seguro action = recebe vai o formulário -->
+    Login <input type="text" id="login" name="login"><br> <!-- login -->
+    Senha <input type="password" id="senha" name="senha"><br> <!-- senha -->
+    <input type="submit" value="Validar"> <!-- botão validar -->
+    
+
+
+
+    </form>
+
+
+
+
+    
+</body>
+</html>
