79e154aee38881faa404bd0c33ace1aa49fc0ad7
Guilherme-G <142443633+Guilherme-G@users.noreply.github.com>
2026-03-26 00:38:52 +0000

    topico 4 aula 25/03/26

---
+++ src/topico041.php
@@ -0,0 +1,12 @@
+
+<?php //Operadores Aritméticos
+$a=10;
+$b=2;
+echo "Adição:".($a + $b); //
+echo "<br>Subtração:".($a - $b);
+echo "<br>Multiplicação:".($a * $b);
+echo "<br>Divisão:".($a / $b);
+echo "<br>Módulo:".($a % $b);
+echo "<br>Exponenciação:".($a ** $b);
+?>
+<hr>
---
+++ src/topico044.php
@@ -0,0 +1,16 @@
+<?php //Operadores de Incremento/Decremento
+$x = 100;
+echo "x = ".$x++; //x = 100
+echo "<br>x final = ".$x; //x final = 101
+
+$i = 10;
+echo "<br>i = $i";
+$i++;//11
+$i++;//12
+++$i;//13
+echo "<br>i = $i";//i = 13
+$i--;//12
+--$i;//11
+echo "<br>i = $i";//i = 11
+
+?>
